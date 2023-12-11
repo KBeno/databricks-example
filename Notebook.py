3347286980ff665f5dc9ab3706385760ce6286a8
benedek.kiss@databricks.com <benedek.kiss@databricks.com>
2023-12-11 07:48:00 +0000

    Notebook functionality

---
+++ Notebook.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Notebook functionality
+# MAGIC
+# MAGIC The purpose of this notebook is to demonstrate notebook functionality in Databricks
+
+# COMMAND ----------
+
+print("This is some python code")
